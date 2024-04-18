8ce34ae9dec4a62ee002aa760bb14c3d22a82c7b
jovination <jovination791@gmail.com>
2024-04-19 01:52:44 +0300

    v2.0

--- background.js
+++ background.js
@@ -1,25 +1,22 @@
- chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        return { cancel: true };
-    },
-    {
-        urls: [defaultFilters],
-     },
-    
-    ["blocking"]
- )    
+JavaScript
+const defaultFilters = [
+  "*://*.doubleclick.net/*",
+  "*://*.partner.googleadservices.com/*",
+  "*://*.googlesyndication.com/*",
+  "*://*.ytimg.com/*",
+  "*://*.youtube-nocookie.com/*",
+  "*://*google-analytics.com/*",
+  "*://*.googletagmanager.com/*",
+  "*://*.googletagservices.com/*",
+  "*://*.adbrite.com/*",
+  "*://*.exponential.com/*",
+  "*://*.quantserve.com/*",
+];
 
- defaultFilters = [
-    "*://*.doubleclick.net/*",
-    "*://*.partner.googleadservices.com/*",
-    "*://*.googlesyndication.com/*",
-    "*://*.ytimg.com/*",
-    "*://*.youtube-nocookie.com/*",
-    "*://*google-analytics.com/*",
-    "*://*.googletagmanager.com/*",
-    "*://*.googletagservices.com/*",
-    "*://*.adbrite.com/*",
-    "*://*.exponential.com/*",
-    "*://*.quantserve.com/*",
-
- ]
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    return { cancel: true };
+  },
+  { urls: defaultFilters },
+  ["blocking"]
+);
